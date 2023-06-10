0c92dabfaa05c2e45bfe3e95e620963cd960c61b
mustofa-id <hm.dev7@gmail.com>
2023-06-10 15:50:53 +0700

    fix: don't expose `send` from base api

--- src/base.ts
+++ src/base.ts
@@ -5,7 +5,7 @@ export abstract class BaseApi<T extends Type = Type> {
 
 	constructor(private readonly fetcher: Fetcher) {}
 
-	send<R>(option: SendOption) {
+	protected send<R>(option: SendOption) {
 		return this.fetcher.send<T, R>(this.type, option);
 	}
 }
