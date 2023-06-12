5d7e98e14ecee110a55b7d676e2e06e826abc0ec
mustofa-id <hm.dev7@gmail.com>
2023-06-12 13:18:29 +0700

    tidy up `generateDateRanges()` util

--- test/utils.ts
+++ test/utils.ts
@@ -12,9 +12,9 @@ export function generateDateRanges(year: number, range = 90): [string, string][]
 	const currentDate = new Date(startDate);
 
 	while (currentDate <= endDate) {
-		const rangeStart = currentDate.toISOString().slice(0, 10); // Format as 'YYYY-MM-DD'
-		currentDate.setDate(currentDate.getDate() + (range - 1)); // Add 89 days to the current date
-		const rangeEnd = currentDate.toISOString().slice(0, 10);
+		const rangeStart = currentDate.toISOString().split('T')[0];
+		currentDate.setDate(currentDate.getDate() + (range - 1));
+		const rangeEnd = currentDate.toISOString().split('T')[0];
 		dates.push([rangeStart, rangeEnd]);
 		currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
 	}
