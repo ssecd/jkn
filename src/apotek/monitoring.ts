a83c09bf3727aeaf4c9785eac487699b20fd86ad
mustofa-id <hm.dev7@gmail.com>
2023-06-16 06:21:53 +0700

    apotek monitoring api

---
+++ src/apotek/monitoring.ts
@@ -0,0 +1,42 @@
+import { ApotekBaseApi } from './base.js';
+
+export class Monitoring extends ApotekBaseApi {
+	/**
+	 * Data klaim resep
+	 */
+	async dataKlaim(params: {
+		/** angka bulan 1 sampai 12 */
+		bulan: number;
+
+		/** tahun */
+		tahun: number;
+
+		/** jenis obat (0 = Semua) (1 = Obat PRB) (2 = Obat Kronis Belum Stabil) (3 = Obat Kemoterapi) */
+		jenisObat: number;
+
+		/** status klaim (1 = Belum di-verifikasi) (2 = Sudah Verifikasi) */
+		status: number;
+	}) {
+		return this.send<{
+			rekap: {
+				jumlahdata: string;
+				totalbiayapengajuan: string;
+				totalbiayasetuju: string;
+				listsep: {
+					nosepapotek: string;
+					nosepaasal: string;
+					nokartu: string;
+					namapeserta: string;
+					noresep: string;
+					jnsobat: string;
+					tglpelayanan: string;
+					biayapengajuan: string;
+					biayasetuju: string;
+				}[];
+			};
+		}>({
+			path: `/monitoring/klaim/${params.bulan}/${params.tahun}/${params.jenisObat}/${params.status}`,
+			method: 'GET'
+		});
+	}
+}
