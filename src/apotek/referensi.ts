073116e8d735be3c594c3e5252cc04e2420edc28
mustofa-id <hm.dev7@gmail.com>
2023-06-16 08:46:50 +0700

    apotek referensi api

---
+++ src/apotek/referensi.ts
@@ -0,0 +1,130 @@
+import { ApotekBaseApi } from './base.js';
+
+export class Referensi extends ApotekBaseApi {
+	/**
+	 * Daftar obat DPHO
+	 */
+	async dpho() {
+		return this.send<{
+			list: {
+				kodeobat: string;
+				namaobat: string;
+				prb: 'True' | 'False';
+				kronis: 'True' | 'False';
+				kemo: 'True' | 'False';
+				harga: string;
+				restriksi: string;
+				generik: string;
+				aktif: string | null;
+			}[];
+		}>({
+			path: `/referensi/dpho`,
+			method: 'GET'
+		});
+	}
+
+	/**
+	 * Daftar referensi poli
+	 */
+	async poli(params: {
+		/** kata kunci kode atau nama poli */
+		keyword: string;
+	}) {
+		return this.send<{
+			poli: {
+				kode: string;
+				nama: string;
+			}[];
+		}>({
+			path: `/referensi/poli/${params.keyword}`,
+			method: 'GET'
+		});
+	}
+
+	/**
+	 * Pencarian data fasilitas kesehatan
+	 */
+	async faskes(params: {
+		/**
+		 * jenis faskes
+		 * - 1 = FKTP atau faskes tingkat 1
+		 * - 2 = FKRTL atau faskes tingkat 2
+		 */
+		jenis: number;
+
+		/** nama atau penggalan kata dari nama faskes */
+		nama: string;
+	}) {
+		return this.send<{
+			faskes: {
+				kode: string;
+				nama: string;
+			}[];
+		}>({
+			path: `/referensi/ppk/${params.jenis}/${params.nama}`,
+			method: 'GET'
+		});
+	}
+
+	/**
+	 * Pencarian setting apotek
+	 */
+	async settingApotek(params: {
+		/** kode apotek */
+		kodeApotek: string;
+	}) {
+		return this.send<{
+			kode: string;
+			namaapoteker: string;
+			namakepala: string;
+			jabatankepala: string;
+			nipkepala: string;
+			siup: string;
+			alamat: string;
+			kota: string;
+			namaverifikator: string;
+			nppverifikator: string;
+			namapetugasapotek: string;
+			nippetugasapotek: string;
+			checkstock: 'True' | 'False';
+		}>({
+			path: `/referensi/settingppk/read/${params.kodeApotek}`,
+			method: 'GET'
+		});
+	}
+
+	/**
+	 * Data spesialistik
+	 */
+	async spesialistik() {
+		return this.send<{
+			list: {
+				kode: string;
+				nama: string;
+			}[];
+		}>({
+			path: `/referensi/spesialistik`,
+			method: 'GET'
+		});
+	}
+
+	/**
+	 * Pencarian data obat
+	 */
+	async obat(param: {
+		/** kode jenis obat */
+		jenis: string;
+
+		/** tanggal resep dengan format YYYY-MM-DD */
+		tanggal: string;
+
+		/** filter nama obat */
+		filter?: string;
+	}) {
+		const filter = param.filter ?? '';
+		return this.send({
+			path: `/referensi/obat/${param.jenis}/${param.tanggal}/${filter}`,
+			method: 'GET'
+		});
+	}
+}
