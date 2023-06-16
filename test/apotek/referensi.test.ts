492ab124223e258beed3339c7856847e4cf18b93
mustofa-id <hm.dev7@gmail.com>
2023-06-16 09:36:59 +0700

    apotek referensi api test

---
+++ test/apotek/referensi.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from 'vitest';
+import jkn from '../jkn';
+
+describe(
+	'Apotek - Referensi',
+	() => {
+		it.concurrent('dpho() - 201 no data', async () => {
+			const result = await jkn.apotek.referensi.dpho();
+			expect(result.metaData.code).toBe('201');
+		});
+
+		it.concurrent('poli() - 200 ok', async () => {
+			const result = await jkn.apotek.referensi.poli({ keyword: 'mata' });
+			expect(result.metaData.code).toBe('200');
+			expect(result.response?.poli.map((d) => d.kode)).toContain('MAT');
+		});
+
+		it.concurrent('faskes() - 200 ok', async () => {
+			const result = await jkn.apotek.referensi.faskes({ jenis: 2, nama: 'mata' });
+			expect(result.metaData.code).toBe('200');
+			expect(result.response?.faskes.map((d) => d.nama)).toContain(
+				'KU MATA SILAMPARI SRIWIJAYA EC - KOTA LUBUK LINGGAU'
+			);
+		});
+
+		it.concurrent('settingApotek() - 201 no data', async () => {
+			const result = await jkn.apotek.referensi.settingApotek({ kodeApotek: '12345' });
+			expect(result.metaData.code).toBe('201');
+		});
+
+		it.concurrent('spesialistik() - 200 ok', async () => {
+			const result = await jkn.apotek.referensi.spesialistik();
+			expect(result.metaData.code).toBe('200');
+			expect(result.response?.list?.[0]?.kode).not.toBeFalsy();
+		});
+
+		it.concurrent('obat() - 201 no data', async () => {
+			const result = await jkn.apotek.referensi.obat({
+				jenis: '12345',
+				tanggal: '2023-01-01'
+			});
+			expect(result.metaData.code).toBe('201');
+		});
+	},
+	{ timeout: 25_000 }
+);
