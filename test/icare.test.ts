ac79155b05ab98259341f67ef6d39a48f037d5f7
mustofa-id <hm.dev7@gmail.com>
2023-08-26 06:22:52 +0700

    clarify test

--- test/icare.test.ts
+++ test/icare.test.ts
@@ -9,16 +9,15 @@ describe(
 				param: '0002084717968',
 				kodedokter: 292667
 			});
-			console.log('result:', result);
-			expect(result.metaData.code).toBe('200');
+			expect(result.metaData.code).toBe(200);
 			expect(result.response?.url).not.toBeFalsy();
+			expect(result.response?.url).contains('?token=');
 		});
 
 		it.concurrent('fktp()', async () => {
 			const result = await jkn.icare.fktp({
 				param: '0002084717968'
 			});
-			console.log('result:', result);
 			expect(result.metaData.code).toBe('200');
 			expect(result.response?.url).not.toBeFalsy();
 		});
