9927022543f77c148dd78777a5c1d6e40c00bcb8
mustofa-id <hm.dev7@gmail.com>
2023-10-24 19:16:53 +0700

    add rekam medis utils test

--- test/rekam-medis/utils.test.ts
+++ test/rekam-medis/utils.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { unzipSync } from 'zlib';
-import { gzip } from '../../src/rekam-medis/utils';
+import { Config } from '../../src/fetcher';
+import { encrypt, gzip } from '../../src/rekam-medis/utils';
 
 describe('utils', () => {
 	it.concurrent('gzip()', async () => {
@@ -10,5 +11,20 @@ describe('utils', () => {
 
 		const unzipped = unzipSync(result);
 		expect(unzipped.toString('utf8')).toBe(value);
+	});
+
+	it.concurrent('decrypt()', async () => {
+		const config = {
+			consId: '12345',
+			consSecret: '53cREt',
+			ppkCode: '54321'
+		};
+
+		const result1 = encrypt('data1', <Config>config);
+		const result2 = encrypt('data2', <Config>config);
+
+		expect(result1.length % 4).toBe(0);
+		expect(result2.length % 4).toBe(0);
+		expect(result1).not.toBe(result2);
 	});
 });
