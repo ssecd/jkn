4909e160508d3d7e6e4388fac0a5b2b7e08ef20e
mustofa-id <hm.dev7@gmail.com>
2023-12-23 10:07:19 +0700

    add doc comments

--- src/aplicares.ts
+++ src/aplicares.ts
@@ -3,6 +3,9 @@ import { BaseApi } from './base.js';
 export class Aplicares extends BaseApi<'aplicares'> {
 	protected type = 'aplicares' as const;
 
+	/**
+	 * Referensi Kamar
+	 */
 	async refKamar() {
 		return this.send<
 			{
@@ -19,14 +22,37 @@ export class Aplicares extends BaseApi<'aplicares'> {
 		});
 	}
 
+	/**
+	 * Update Ketersediaan Tempat Tidur
+	 *
+	 * Property `tersediapria`, `tersediawanita`, dan `tersediapriawanita`
+	 * digunakan untuk faskes yang ingin mencantumkan informasi ketersediaan
+	 * tempat tidur untuk pasien laki-laki, perempuan, dan laki–laki atau
+	 * perempuan.
+	 */
 	async updateBed(data: {
+		/** kode kelas ruang rawat sesuai dengan mapping BPJS Kesehatan */
 		kodekelas: string;
+
+		/** kode ruangan faskes */
 		koderuang: string;
+
+		/** nama ruang rawat faskes */
 		namaruang: string;
+
+		/** kapasitas ruang faskes */
 		kapasitas: number;
+
+		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru */
 		tersedia: number;
+
+		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru laki – laki */
 		tersediapria?: number;
+
+		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru perempuan */
 		tersediawanita?: number;
+
+		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru laki – laki atau perempuan */
 		tersediapriawanita?: number;
 	}) {
 		const { ppkCode } = await this.requiredConfig('ppkCode');
