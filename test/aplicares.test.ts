c437abc2ae57204e5c5f9b2c0520a93bf2e2e4de
mustofa-id <hm.dev7@gmail.com>
2023-12-23 10:48:23 +0700

    don't run test concurrently

--- test/aplicares.test.ts
+++ test/aplicares.test.ts
@@ -1,48 +1,44 @@
 import { describe, expect, it } from 'vitest';
 import jkn from './jkn';
 
-describe(
-	'Aplicares',
-	() => {
-		it.concurrent('refKamar()', async () => {
-			const { response, metadata } = await jkn.aplicares.refKamar();
-			expect(response?.list.length).toBe(metadata.totalitems);
-		});
+describe('Aplicares', () => {
+	it('refKamar()', async () => {
+		const { response, metadata } = await jkn.aplicares.refKamar();
+		expect(response?.list.length).toBe(metadata.totalitems);
+	});
 
-		it.concurrent('create()', async () => {
-			const { metadata } = await jkn.aplicares.create({
-				kodekelas: 'VIP',
-				koderuang: 'R0001',
-				namaruang: 'Ruang Sun VIP',
-				kapasitas: 5,
-				tersedia: 3
-			});
-			expect(metadata.code).toBe(1);
+	it('create()', async () => {
+		const { metadata } = await jkn.aplicares.create({
+			kodekelas: 'VIP',
+			koderuang: 'R0001',
+			namaruang: 'Ruang Sun VIP',
+			kapasitas: 5,
+			tersedia: 3
 		});
+		expect(metadata.code).toBe(1);
+	});
 
-		it.concurrent('update()', async () => {
-			const { metadata } = await jkn.aplicares.update({
-				kodekelas: 'VIP',
-				koderuang: 'R0001',
-				namaruang: 'Ruang Sun VIP',
-				kapasitas: 5,
-				tersedia: 1
-			});
-			expect(metadata.code).toBe(1);
+	it('update()', async () => {
+		const { metadata } = await jkn.aplicares.update({
+			kodekelas: 'VIP',
+			koderuang: 'R0001',
+			namaruang: 'Ruang Sun VIP',
+			kapasitas: 5,
+			tersedia: 1
 		});
+		expect(metadata.code).toBe(1);
+	});
 
-		it.concurrent('read()', async () => {
-			const { response, metadata } = await jkn.aplicares.read({ start: 1, limit: 2 });
-			expect(response?.list.length).toBe(metadata.totalitems);
-		});
+	it('read()', async () => {
+		const { response, metadata } = await jkn.aplicares.read({ start: 1, limit: 2 });
+		expect(response?.list.length).toBe(metadata.totalitems);
+	});
 
-		it.concurrent('delete()', async () => {
-			const { metadata } = await jkn.aplicares.delete({
-				kodekelas: 'VIP',
-				koderuang: 'R0001'
-			});
-			expect(metadata.code).toBe(1);
+	it('delete()', async () => {
+		const { metadata } = await jkn.aplicares.delete({
+			kodekelas: 'VIP',
+			koderuang: 'R0001'
 		});
-	},
-	{ timeout: 25_000 }
-);
+		expect(metadata.code).toBe(1);
+	});
+});
