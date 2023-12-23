c0352ebdf8d384520b2287461a45d3146db9eaab
mustofa-id <hm.dev7@gmail.com>
2023-12-23 10:36:35 +0700

    add create and update test

--- test/aplicares.test.ts
+++ test/aplicares.test.ts
@@ -9,6 +9,28 @@ describe(
 			expect(response?.list.length).toBe(metadata.totalitems);
 		});
 
+		it.concurrent('create()', async () => {
+			const { metadata } = await jkn.aplicares.create({
+				kodekelas: 'VIP',
+				koderuang: 'R0001',
+				namaruang: 'Ruang Sun VIP',
+				kapasitas: 5,
+				tersedia: 3
+			});
+			expect(metadata.code).toBe(1);
+		});
+
+		it.concurrent('update()', async () => {
+			const { metadata } = await jkn.aplicares.update({
+				kodekelas: 'VIP',
+				koderuang: 'R0001',
+				namaruang: 'Ruang Sun VIP',
+				kapasitas: 5,
+				tersedia: 1
+			});
+			expect(metadata.code).toBe(1);
+		});
+
 		it.concurrent('read()', async () => {
 			const { response, metadata } = await jkn.aplicares.read({ start: 1, limit: 2 });
 			console.log(metadata, response);
