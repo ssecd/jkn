58301679f8b576918749515bebea011ab24a5e92
mustofa-id <hm.dev7@gmail.com>
2023-12-23 10:15:30 +0700

    add create ruang method

--- src/aplicares.ts
+++ src/aplicares.ts
@@ -30,31 +30,7 @@ export class Aplicares extends BaseApi<'aplicares'> {
 	 * tempat tidur untuk pasien laki-laki, perempuan, dan laki–laki atau
 	 * perempuan.
 	 */
-	async updateBed(data: {
-		/** kode kelas ruang rawat sesuai dengan mapping BPJS Kesehatan */
-		kodekelas: string;
-
-		/** kode ruangan faskes */
-		koderuang: string;
-
-		/** nama ruang rawat faskes */
-		namaruang: string;
-
-		/** kapasitas ruang faskes */
-		kapasitas: number;
-
-		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru */
-		tersedia: number;
-
-		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru laki – laki */
-		tersediapria?: number;
-
-		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru perempuan */
-		tersediawanita?: number;
-
-		/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru laki – laki atau perempuan */
-		tersediapriawanita?: number;
-	}) {
+	async updateBed(data: AplicaresBedData) {
 		const { ppkCode } = await this.requiredConfig('ppkCode');
 		return this.send({
 			path: `/rest/bed/update/${ppkCode}`,
@@ -62,4 +38,47 @@ export class Aplicares extends BaseApi<'aplicares'> {
 			data
 		});
 	}
+
+	/**
+	 * Buat Ruangan Baru
+	 *
+	 * Property `tersediapria`, `tersediawanita`, dan `tersediapriawanita`
+	 * digunakan untuk faskes yang ingin mencantumkan informasi ketersediaan
+	 * tempat tidur untuk pasien laki-laki, perempuan, dan laki–laki atau
+	 * perempuan.
+	 */
+	async createRuang(data: AplicaresBedData) {
+		const { ppkCode } = await this.requiredConfig('ppkCode');
+		return this.send({
+			path: `/rest/bed/create/${ppkCode}`,
+			method: 'POST',
+			data
+		});
+	}
+}
+
+interface AplicaresBedData {
+	/** kode kelas ruang rawat sesuai dengan mapping BPJS Kesehatan */
+	kodekelas: string;
+
+	/** kode ruangan faskes */
+	koderuang: string;
+
+	/** nama ruang rawat faskes */
+	namaruang: string;
+
+	/** kapasitas ruang faskes */
+	kapasitas: number;
+
+	/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru */
+	tersedia: number;
+
+	/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru laki – laki */
+	tersediapria?: number;
+
+	/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru perempuan */
+	tersediawanita?: number;
+
+	/** jumlah tempat tidur yang kosong atau dapat ditempati pasien baru laki – laki atau perempuan */
+	tersediapriawanita?: number;
 }
