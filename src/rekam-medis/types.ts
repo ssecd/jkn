1fe86668720e2e89e989ba51fd187f600cfe132f
mustofa-id <hm.dev7@gmail.com>
2024-01-16 08:30:28 +0700

    type: composition

--- src/rekam-medis/types.ts
+++ src/rekam-medis/types.ts
@@ -1,21 +1,61 @@
-export interface Composition {}
+interface Coding {
+	use: string;
+	system: string;
+	code: string;
+	display: string;
+}
 
-export interface Patient  {}
+interface Reference {
+	reference: string;
+	display: string;
+}
+
+export interface Composition {
+	resourceType: 'Composition';
+	id: string;
+	status: string;
+	type: {
+		coding: Coding[];
+		text: string;
+	};
+	subject: Reference;
+	encounter: Reference;
+	date: string;
+	author: Reference[];
+	title: string;
+	confidentiality: string;
+	section: {
+		[key: string]: {
+			title: string;
+			code: {
+				coding: Coding[];
+			};
+			text: {
+				status: string;
+				div: string;
+			};
+			mode: string;
+			entry: Reference[];
+		};
+	};
+}
+
+export interface Patient {}
 
 export interface Encounter {}
 
-export interface MedicationRequest  {}
+export interface MedicationRequest {}
 
-export interface Practitioner  {}
+export interface Practitioner {}
 
-export interface Organization  {}
+export interface Organization {}
 
-export interface Condition  {}
+export interface Condition {}
 
-export interface DiagnosticReport  {}
+export interface DiagnosticReport {}
 
-export interface Procedure  {}
+export interface Procedure {}
 
-export interface Device  {}
+export interface Device {}
 
 export interface Bundle {}
