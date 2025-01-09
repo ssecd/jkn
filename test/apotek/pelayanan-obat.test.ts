1e6c5eadf5e41f1689a9ee288ea9cb8c3047092b
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-01-09 12:24:34 +0700

    format

--- test/apotek/pelayanan-obat.test.ts
+++ test/apotek/pelayanan-obat.test.ts
@@ -1,24 +1,20 @@
 import { describe, expect, it } from 'vitest';
 import jkn from '../jkn';
 
-describe(
-	'Apotek - Pelayanan Obat',
-	{ timeout: 25_000 },
-	() => {
-		it.concurrent('daftar() - 201 no data', async () => {
-			const result = await jkn.apotek.pelayananObat.daftar({
-				nomorSep: '0089S0020523V000001'
-			});
-			expect(result.metaData.code).toBe('201');
+describe('Apotek - Pelayanan Obat', { timeout: 25_000 }, () => {
+	it.concurrent('daftar() - 201 no data', async () => {
+		const result = await jkn.apotek.pelayananObat.daftar({
+			nomorSep: '0089S0020523V000001'
 		});
+		expect(result.metaData.code).toBe('201');
+	});
 
-		it.concurrent('riwayat() - 201 no data', async () => {
-			const result = await jkn.apotek.pelayananObat.riwayat({
-				awal: '2023-01-01',
-				akhir: '2023-01-31',
-				nomorKartu: '0002084717968'
-			});
-			expect(result.metaData.code).toBe('201');
+	it.concurrent('riwayat() - 201 no data', async () => {
+		const result = await jkn.apotek.pelayananObat.riwayat({
+			awal: '2023-01-01',
+			akhir: '2023-01-31',
+			nomorKartu: '0002084717968'
 		});
-	}
-);
+		expect(result.metaData.code).toBe('201');
+	});
+});
