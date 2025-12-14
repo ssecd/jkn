2a3a2d4da7cff1d27b2eff67bf2a605512998e4f
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-12-14 07:55:57 +0700

    handle optional param

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -126,7 +126,7 @@ export interface Config {
 export interface SendOption {
 	/** name of request, it helpful for log or collect stats */
 	name?: string;
-	path: `/${string}` | [`/${string}`, Record<string, string | number>];
+	path: `/${string}` | [`/${string}`, Record<string, string | number | undefined | null>];
 	method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
 	data?: unknown;
 	skipDecrypt?: boolean;
@@ -314,31 +314,13 @@ export class Fetcher {
 		return lz.decompressFromEncodedURIComponent(text);
 	}
 
-	private normalizePath(path: SendOption['path']) {
-		const [pathname, params] = typeof path == 'string' ? [path] : path;
-		if (!pathname.startsWith('/')) throw new Error(`Path must start with "/"`);
-		if (!params) return pathname;
-
-		return pathname.replace(/:([A-Za-z0-9_]+)/g, (_, key) => {
-			if (!(key in params)) throw new Error(`Missing params: ${key}`);
-
-			const value = String(params[key]);
-			try {
-				// In case the value is already encoded by the user
-				return encodeURIComponent(decodeURIComponent(value));
-			} catch {
-				return encodeURIComponent(value);
-			}
-		});
-	}
-
 	async send<T extends Type, R, M>(
 		type: T,
 		option: SendOption
 	): Promise<SendResponse<R | undefined, M | undefined>[T]> {
 		await this.applyConfig();
 
-		const path = this.normalizePath(option.path);
+		const path = normalizePath(option.path);
 		const baseUrl = this.config.baseUrls[type];
 		if (!baseUrl) throw new Error(`Invalid base URL for type "${type}"`);
 
@@ -431,4 +413,27 @@ function parseHtml(html?: string) {
 		.trim()
 		.replace(/\r?\n+/g, ' - ') // newlines to dash
 		.replace(/\s+/g, ' '); // normalize whitespace
+}
+
+export function normalizePath(path: SendOption['path']) {
+	const [pathname, params] = typeof path == 'string' ? [path] : path;
+
+	if (!pathname.startsWith('/')) throw new Error(`Path must start with "/"`);
+	if (!params) return pathname;
+
+	return pathname.replace(/\/:([A-Za-z0-9_]+)(\?)?/g, (_, key: string, optional?: string) => {
+		const value = params[key];
+
+		if (value == null) {
+			if (optional) return ''; // remove entire `/:param?`
+			throw new Error(`Missing params: ${key}`);
+		}
+
+		const component = String(value);
+		try {
+			return '/' + encodeURIComponent(decodeURIComponent(component));
+		} catch {
+			return '/' + encodeURIComponent(component);
+		}
+	});
 }
